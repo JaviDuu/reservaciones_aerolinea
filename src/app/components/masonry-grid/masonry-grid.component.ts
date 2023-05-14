689c8eceea322a808d7b980e9c00a6a699fa1a9f
Javier Guerrero <javier.guerrero.c2001@gmail.com>
2023-05-13 21:32:29 -0600

    imagenes

--- src/app/components/masonry-grid/masonry-grid.component.ts
+++ src/app/components/masonry-grid/masonry-grid.component.ts
@@ -10,16 +10,16 @@ export class MasonryGridComponent {
   @Output() outputData = new EventEmitter<any[]>();
 
   images = [
-    { url: 'https://drive.google.com/file/d/1w_e_hiAgYEf4uraZ9IKts6QUhg9mc3p5/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1fultATpGPeRyxAh52PoO2_zuFoNtp38X/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/12G20kjkrfsrOg5C7vfRei16KlbfzPd0x/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/13JSVu-9s5V1LIuIB_AyY3XFB52QfsXgf/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1DpkVm-JBmcSXXcQiHQJfWQr4znFOfM43/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1UwqZnwYvXYaLHJASR1WCTsIRa_60xgOO/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1lYMfp3Qtfb3wO9Gj1diS1A-MfgTkcMSq/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1NCRqr0oM8_wn59WQtmZN0djgFP5ZCj4O/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/13Zr_yuJODDlgXe6OXEJ_TjmoOo1uNgdj/view?usp=share_link' },
-    { url: 'https://drive.google.com/file/d/1l6UlEIaj7UsXDytTqP3ry_iZc9j4ikjx/view?usp=share_link' },
+    { url: 'assets/imagenes/img1.jpg' },
+    { url: 'assets/imagenes/img5.jpg' },
+    { url: 'assets/imagenes/img6.jpg' },
+    { url: 'assets/imagenes/img10.jpg' },
+    { url: 'assets/imagenes/img9.jpg' },
+    { url: 'assets/imagenes/img2.jpg' },
+    { url: 'assets/imagenes/img8.jpg' },
+    { url: 'assets/imagenes/img3.jpg' },
+    { url: 'assets/imagenes/img7.jpg' },
+    { url: 'assets/imagenes/img4.jpg' },
   ];
 
   ngOnInit() {
